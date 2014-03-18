1233b20149709c7479a6af92615905d8deab9b94
Vitaly Parfonov <vparfonov@codenvy.com>
2014-03-18 16:38:36 +0200

    rename catalina.bat codenvy.bat

--- assembly-sdk/src/main/resources/tomcat/bin/setenv.bat
+++ assembly-sdk/src/main/resources/tomcat/bin/setenv.bat
@@ -21,8 +21,7 @@
 set IDE_OPTS="-Dorg.exoplatform.logreader.logpath=%CATALINA_HOME%/logs/logreader"
 set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
 set CODENVY_CONFIG_OPTS="-Xshare:auto -Xms512m -Xmx1024m -XX:MaxPermSize=256m"
-set CODENVY_OPTS="-Djavasrc=%JAVA_HOME%\src.zip" "-Djre.lib=%JAVA_HOME%\jre\lib" "-Dorg.exoplatform.mimetypes=conf\mimetypes.properties" "-Dorg.exoplatform.ide.git.server=git" "-Dorg.exoplatform.ide.server.fs-root-path=${CATALINA_HOME}/temp/fs-root" "-Dtenant.masterhost=localhost" "-Dorganization.application.server.url=http://localhost:8080/userdb" "-Dmailsender.application.server.url=http://localhost:8080/mail" "-Dbuilder.timeout=600" "-Dcodenvy.local.conf.dir=${CATALINA_HOME}/conf"
-set JAVA_OPTS=%LOG_OPTS% %CODENVY_CONFIG_OPTS% %CODENVY_OPTS% %IDE_OPTS%
+set JAVA_OPTS=%LOG_OPTS% %CODENVY_CONFIG_OPTS% %IDE_OPTS%
 set CLASSPATH=%CATALINA_HOME%/conf/;%CATALINA_HOME%/lib/jul-to-slf4j.jar;%CATALINA_HOME%/lib/slf4j-api.jar;%CATALINA_HOME%/lib/logback-classic.jar;%CATALINA_HOME%/lib/logback-core.jar;%JAVA_HOME%/lib/tools.jar
 echo "======="
 echo %JAVA_OPTS%
--- assembly-sdk/src/main/resources/tomcat/bin/setenv.sh
+++ assembly-sdk/src/main/resources/tomcat/bin/setenv.sh
@@ -16,27 +16,9 @@
 # from Codenvy S.A..
 #
 
-IDE_OPTS="          -Dorg.exoplatform.logreader.logpath=${CATALINA_HOME}/logs/logreader"
-
-LOG_OPTS="          -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
-
-CODENVY_CONFIG_OPTS="   -Xshare:auto -Xms512m -Xmx1024m -XX:MaxPermSize=256m"
-
-JAVA_SRC="          $JAVA_HOME/src.zip"
-
-CODENVY_OPTS="      -Djavasrc=$JAVA_HOME/src.zip \
-                    -Djre.lib=$JAVA_HOME/jre/lib \
-                    -Dorg.exoplatform.mimetypes=conf/mimetypes.properties \
-                    -Dorg.exoplatform.ide.git.server=git \
-                    -Dorg.exoplatform.ide.server.fs-root-path=${CATALINA_HOME}/temp/fs-root \
-                    -Dtenant.masterhost=localhost \
-                    -Dorganization.application.server.url=http://localhost:8080/userdb/ \
-                    -Dmailsender.application.server.url=http://localhost:8080/mail/ \
-                    -Dcodenvy.local.conf.dir=${CATALINA_HOME}/conf"
-
+LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
+CODENVY_CONFIG_OPTS="-Xshare:auto -Xms512m -Xmx1024m -XX:MaxPermSize=256m"
 #REMOTE_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-
-JAVA_OPTS="$JAVA_OPTS $LOG_OPTS $CODENVY_CONFIG_OPTS $CODENVY_OPTS $REMOTE_DEBUG $IDE_OPTS"
-
+JAVA_OPTS="$JAVA_OPTS $LOG_OPTS $CODENVY_CONFIG_OPTS $REMOTE_DEBUG"
 export JAVA_OPTS
 export CLASSPATH="${CATALINA_HOME}/conf/:${CATALINA_HOME}/lib/jul-to-slf4j.jar:${CATALINA_HOME}/lib/slf4j-api.jar:${CATALINA_HOME}/lib/logback-classic.jar:${CATALINA_HOME}/lib/logback-core.jar:${JAVA_HOME}/lib/tools.jar"
