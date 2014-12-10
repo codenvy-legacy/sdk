8330b78cc6d9068a70f4b1b4a33dfcb215b0d3ba
Evgen Vidolob <evidolob@codenvy.com>
2014-12-10 10:55:34 +0200

    IDEX-1601 add javadoc service

--- assembly-java-codeassistant/src/main/java/com/codenvy/ide/ext/java/server/JavaCodeAssistantModule.java
+++ assembly-java-codeassistant/src/main/java/com/codenvy/ide/ext/java/server/JavaCodeAssistantModule.java
@@ -26,6 +26,7 @@ public class JavaCodeAssistantModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(RestNameEnvironment.class);
+        bind(JavadocService.class);
         bind(JavaProjectWatcherService.class);
         bind(AsynchronousJobPool.class).to(CodenvyAsynchronousJobPool.class);
         bind(new PathKey<>(AsynchronousJobService.class, "/async/{ws-id}")).to(AsynchronousJobService.class);
